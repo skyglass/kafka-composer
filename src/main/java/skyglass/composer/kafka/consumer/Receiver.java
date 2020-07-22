ba6d8fe1ec69d6a297ead788d34654be91ba6e9a
Mykhailo Skliar <mykhailo.skliar@c-com.net>
2020-07-22 11:45:34 +0200

    KFC-002
    - implemented simple kafka spring boot application with producer and
    consumer. The "publish" endpoint is available in swagger ui. The kafka
    server is started with docker-compose

--- src/main/java/skyglass/composer/kafka/consumer/Receiver.java
+++ src/main/java/skyglass/composer/kafka/consumer/Receiver.java
@@ -5,23 +5,21 @@ import java.util.concurrent.CountDownLatch;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
 
-@Component
+// @Component
 public class Receiver {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
 
-  private CountDownLatch latch = new CountDownLatch(1);
+	private CountDownLatch latch = new CountDownLatch(1);
 
-  public CountDownLatch getLatch() {
-    return latch;
-  }
+	public CountDownLatch getLatch() {
+		return latch;
+	}
 
-  @KafkaListener(topics = "${topic.boot}")
-  public void receive(ConsumerRecord<?, ?> consumerRecord) {
-    LOGGER.info("received data='{}'", consumerRecord.toString());
-    latch.countDown();
-  }
+	//@KafkaListener(topics = "users")
+	public void receive(ConsumerRecord<?, ?> consumerRecord) {
+		LOGGER.info("received data='{}'", consumerRecord.toString());
+		latch.countDown();
+	}
 }
--- src/main/java/skyglass/composer/kafka/producer/Sender.java
+++ src/main/java/skyglass/composer/kafka/producer/Sender.java
@@ -4,18 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
 
-@Component
+// @Component
 public class Sender {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(Sender.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(Sender.class);
 
-  @Autowired
-  private KafkaTemplate<String, String> kafkaTemplate;
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
 
-  public void send(String topic, String data) {
-    LOGGER.info("sending data='{}' to topic='{}'", data, topic);
-    kafkaTemplate.send(topic, data);
-  }
+	public void send(String topic, String data) {
+		LOGGER.info("sending data='{}' to topic='{}'", data, topic);
+		kafkaTemplate.send(topic, data);
+	}
 }
